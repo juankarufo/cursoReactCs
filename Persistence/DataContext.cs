c9ae09069d6a4d012eda6ade40b4aff6aea2cb1e
JuanCa <>
2023-09-09 16:08:02 +0200

    s2 - add dbcontext
    addedpackage Microsoft.EntityFrameworkCore.Sqlite and code for using it. Do not forget to execute "dotnet restore" once the package is added to be available from Program.cs in API project

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    internal class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Activity> MyProperty { get; set; }
+    }
+}
