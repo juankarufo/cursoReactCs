610d4f635fdba2ded3f24462ae201dd904e090f9
JuanCa <>
2023-09-09 16:50:19 +0200

    s2 - first migration
    add the package "Microsoft.EntityFrameworkCore.Design" to API. Use "dotnet tool update --global dotnet-ef" to avoid messages with different versions and dotnet restore if there is any other problem.
    
    To create the migration execute "dotnet ef migrations add InitialCreate -s API -p Persistence --context DataContext" where "DataContext" is the name of the class (which MUST be public hehe)

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Persistence
 {
-    internal class DataContext : DbContext
+    public class DataContext : DbContext
     {
         public DataContext(DbContextOptions options) : base(options)
         {
