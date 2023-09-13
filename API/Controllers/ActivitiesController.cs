fbbaee06bf3626f51dea98a9c68c7103fd121c26
Juan Carlos de la Coba <jdel@ciklum.com>
2023-09-13 19:00:51 +0200

    s4 - add id finder using MediatR, Query and Handler

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -16,7 +16,7 @@ namespace API.Controllers
         [HttpGet("{id}")] // api/activities/{GUID}
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
-            return Ok();
+            return await Mediator.Send(new Details.Query {Id = id });
         }
     }
 }
