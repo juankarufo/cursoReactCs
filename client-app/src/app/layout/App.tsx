2673a03bce1c276eeba695cdda9fa0eb94bfef2c
Juan Carlos de la Coba <jdel@ciklum.com>
2023-11-10 16:22:18 +0100

    s5 - add new activities with uuid
    it is needed to install "npm install uuid" and if there is a warning importing use "npm i --save-dev @types/uuid"

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -6,6 +6,7 @@ import 'semantic-ui-css/semantic.min.css'
 import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
+import {v4 as uuid} from 'uuid';
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
@@ -40,7 +41,7 @@ function App() {
   function handleCreateOrEditActivity(activity: Activity){
     activity.id 
     ? setActivities([...activities.filter( x => x.id !== activity.id), activity])
-    : setActivities([...activities, activity]);
+    : setActivities([...activities, {...activity, id: uuid()}]);
 
     setEditMode(false);
     setSelectedActivity(activity);
