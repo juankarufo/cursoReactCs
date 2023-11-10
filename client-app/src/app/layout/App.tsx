abc4fa45b072bf41fd267ecf6520c5a03dfb326d
Juan Carlos de la Coba <jdel@ciklum.com>
2023-11-10 17:13:31 +0100

    Revert "s6 - create basic axios agent"
    This reverts commit 0724c0e09a00046a4bf59337df12998ba16e5a7b.

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,12 +1,12 @@
 
 import { Fragment, useEffect, useState } from 'react'
+import axios from 'axios';
 import { Container } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css'
 import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import {v4 as uuid} from 'uuid';
-import agent from '../api/agent';
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
@@ -14,8 +14,9 @@ function App() {
   const [editMode, setEditMode] = useState(false);
   
   useEffect( () => {
-    agent.Activities.list().then( response => {
-      setActivities(response)
+    axios.get<Activity[]>('http://localhost:5000/api/activities')
+    .then( response => {
+      setActivities(response.data)
     })
   }, [])
 
