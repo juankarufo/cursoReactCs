36d0e634ad546198aaf3ca2a12a7774ac16a3560
Juan Carlos de la Coba <jdel@ciklum.com>
2023-11-14 15:33:42 +0100

    s7 - refactoring to use mobX

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,6 +1,6 @@
 
 import { Fragment, useEffect, useState } from 'react'
-import { Button, Container } from 'semantic-ui-react';
+import { Container } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css'
 import { Activity } from '../models/activity';
 import NavBar from './NavBar';
@@ -22,16 +22,8 @@ function App() {
   const [submitting, setSubmitting] = useState(false);
   
   useEffect( () => {
-    agent.Activities.list().then( response => {
-      let activities: Activity[] = [];
-      response.forEach( activity => {
-        activity.date = activity.date.split('T')[0];
-        activities.push(activity);
-      });
-      setActivities(activities);
-      setLoading(false);
-    })
-  }, [])
+    activityStore.loadActivities();
+  }, [activityStore])
 
   function handleSelectActivity (id: string){
     setSelectedActivity(activities.find(x => x.id === id));
@@ -79,16 +71,14 @@ function App() {
     });
   }
 
-  if(loading) return (<LoadingComponent />)
+  if(activityStore.loadingInitial) return (<LoadingComponent />)
   return (
     <Fragment>
 
       <NavBar openForm={handleFormOpen} />
       <Container style={{marginTop: '7em'}}>
-        <h2>{activityStore.title}</h2>
-        <Button onClick={activityStore.setTitle} content='Exclamation' />
         <ActivityDashboard 
-          activities={activities}
+          activities={activityStore.activities}
           selectedActivity={selectedActivity}
           selectActivity={handleSelectActivity}
           cancelSelectActivity={handleCancelSelectActivity}
