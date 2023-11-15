6c5cff225442405e0f6f37f74654109de049554e
Juan Carlos de la Coba <jdel@ciklum.com>
2023-11-15 12:29:01 +0100

    s8 - adding routes

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -3,27 +3,16 @@ import { Fragment, useEffect } from 'react'
 import { Container } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css'
 import NavBar from './NavBar';
-import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
-import LoadingComponent from './LoadingComponent';
-import { useStore } from '../stores/store';
 import { observer } from 'mobx-react-lite';
+import { Outlet } from 'react-router';
 
 function App() {
-
-  const{activityStore} = useStore();
-  
-  useEffect( () => {
-    activityStore.loadActivities();
-  }, [activityStore])
-
-
-  if(activityStore.loadingInitial) return (<LoadingComponent />)
   return (
     <Fragment>
 
       <NavBar />
       <Container style={{marginTop: '7em'}}>
-        <ActivityDashboard />
+        <Outlet />
       </Container>
       
     </Fragment>
--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -4,6 +4,8 @@ import ActivityDetails from '../detail/ActivityDetails';
 import ActivityForm from '../form/ActivityForm';
 import { useStore } from '../../../app/stores/store';
 import { observer } from 'mobx-react-lite';
+import { useEffect } from 'react';
+import LoadingComponent from '../../../app/layout/LoadingComponent';
 
 
 export default observer(function ActivityDashboard() {
@@ -11,6 +13,14 @@ export default observer(function ActivityDashboard() {
     const {activityStore} = useStore();
     const {selectedActivity, editMode} = activityStore;
     
+  
+    useEffect( () => {
+      activityStore.loadActivities();
+    }, [activityStore])
+  
+  
+    if(activityStore.loadingInitial) return (<LoadingComponent />)
+
     return (
         <Grid>
             <Grid.Column width='10'>
