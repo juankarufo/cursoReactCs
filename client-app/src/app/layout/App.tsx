91b37bfa3de3023f56b3eee6aa04b5713b8a9ba3
Juan Carlos de la Coba <jdel@ciklum.com>
2023-11-20 11:26:59 +0100

    s8 - moving home page outside nav

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -5,16 +5,21 @@ import 'semantic-ui-css/semantic.min.css'
 import NavBar from './NavBar';
 import { observer } from 'mobx-react-lite';
 import { Outlet } from 'react-router';
+import { useLocation } from 'react-router-dom';
+import { HomePage } from '../../features/home/HomePage';
 
 function App() {
+  const location = useLocation();
   return (
     <Fragment>
-
-      <NavBar />
-      <Container style={{marginTop: '7em'}}>
-        <Outlet />
-      </Container>
-      
+      {location.pathname === '/' ? <HomePage /> : (
+        <>
+          <NavBar />
+          <Container style={{marginTop: '7em'}}>
+            <Outlet />
+          </Container>
+        </>
+      ) }
     </Fragment>
   )
 }
