5d1adb1b528c9e63d28112fe80df909f0433617f
Juan Carlos de la Coba <jdel@ciklum.com>
2023-11-20 18:29:16 +0100

    s9 - styling home page

--- client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -1,8 +1,6 @@
-import { Button, Icon, Item, Label, Segment } from "semantic-ui-react";
+import { Button, Icon, Item, Segment } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
-import { SyntheticEvent, useState } from "react";
 import { Link } from "react-router-dom";
-import { useStore } from "../../../app/stores/store";
 
 interface Props {
     activity: Activity
@@ -10,16 +8,6 @@ interface Props {
 
 export default function ActivityListItem ({activity}: Props) {
    
-    const {activityStore} = useStore();
-    const {deleteActivity,  loading} = activityStore;
-    
-    const [target, setTarget] = useState('');
-
-    function handleAActivityDelete(e: SyntheticEvent<HTMLButtonElement>, id: string){
-        setTarget(e.currentTarget.name);
-        deleteActivity(id) 
-    }
-
     return (
         <Segment.Group>
             <Segment>
