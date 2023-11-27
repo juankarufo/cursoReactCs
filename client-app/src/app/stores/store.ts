46d01df78046f36f2de201f78cabd2cd6be7aabe
Juan Carlos de la Coba <jdel@ciklum.com>
2023-11-27 18:17:37 +0100

    s10 - handling error 500 on client side

--- client-app/src/app/stores/store.ts
+++ client-app/src/app/stores/store.ts
@@ -1,12 +1,15 @@
 import { createContext, useContext } from "react";
 import ActivityScore from "./activityStore";
+import CommonStore from "./commonStore";
 
 interface Store {
-    activityStore: ActivityScore
+    activityStore: ActivityScore;
+    commonStore: CommonStore;
 }
 
 export const store: Store = {
-    activityStore: new ActivityScore()
+    activityStore: new ActivityScore(),
+    commonStore: new CommonStore()
 }
 export const StoreContext = createContext(store);
 
