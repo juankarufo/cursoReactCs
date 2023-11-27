441bfa5df5e11c2e37f6bf58efe368142ce1a84f
Juan Carlos de la Coba <jdel@ciklum.com>
2023-11-27 18:47:24 +0100

    s10 - handling bad guid validation error

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -15,9 +15,15 @@ axios.interceptors.response.use (async response => {
         await sleep(600);
         return response;
 }, (error: AxiosError) => {
-    const {data, status} = error.response as AxiosResponse;
+    const {data, status, config} = error.response as AxiosResponse;
     switch(status){
         case 400:
+            //if(config.method === 'get' && data.errors.hasOwnProperty('id')){
+
+            //use this for vite:
+            if(config.method === 'get' && Object.prototype.hasOwnProperty.call(data.errors, 'id')){
+                router.navigate('/not-found'); 
+            }
             if(data.errors){
                 const modalStateErrors = [];
                 for (const key in data.errors){
