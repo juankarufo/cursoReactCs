93e5940abdd5cd66afcb461a865b7f0a12e32026
Juan Carlos de la Coba <jdel@ciklum.com>
2023-12-01 02:22:14 +0100

    s12 - add our AppUser class in our Entity

---
+++ Domain/Appuser.cs
@@ -0,0 +1,11 @@
+﻿
+using Microsoft.AspNetCore.Identity;
+
+namespace Domain
+{
+    public class Appuser : IdentityUser
+    {
+        public string DisplayName { get; set; }
+        public string Bio { get; set; }
+    }
+}
