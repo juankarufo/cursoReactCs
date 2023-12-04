53b04efae376c7544f10324382cf0b7d5342aadb
Juan Carlos de la Coba <jdel@ciklum.com>
2023-12-04 13:53:31 +0100

    s12 - adding DTOs

---
+++ API/DTOs/RegistrerDto.cs
@@ -0,0 +1,10 @@
+﻿namespace API.DTOs
+{
+    public class RegistrerDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string DisplayName { get; set; }
+        public string Username { get; set; }
+    }
+}
