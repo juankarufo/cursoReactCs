8cfa3e28f0adb730c29ddf54ac3f6dced862dff1
Juan Carlos de la Coba <jdel@ciklum.com>
2023-12-04 23:39:24 +0100

    s12 - create user store

--- client-app/src/app/stores/store.ts
+++ client-app/src/app/stores/store.ts
@@ -1,15 +1,18 @@
 import { createContext, useContext } from "react";
 import ActivityScore from "./activityStore";
 import CommonStore from "./commonStore";
+import UserStore from "./userStore";
 
 interface Store {
     activityStore: ActivityScore;
     commonStore: CommonStore;
+    userStore: UserStore;
 }
 
 export const store: Store = {
     activityStore: new ActivityScore(),
-    commonStore: new CommonStore()
+    commonStore: new CommonStore(),
+    userStore: new UserStore()
 }
 export const StoreContext = createContext(store);
 
