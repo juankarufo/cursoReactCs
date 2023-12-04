915ea80ffe10aeaee1b2a9a4b4a782695d59e600
Juan Carlos de la Coba <jdel@ciklum.com>
2023-12-04 18:59:11 +0100

    s12 - register users validation

--- API/DTOs/RegistrerDto.cs
+++ API/DTOs/RegistrerDto.cs
@@ -1,10 +1,18 @@
-﻿namespace API.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs
 {
     public class RegistrerDto
     {
+        [Required]
         public string Email { get; set; }
+        [Required]
+        [EmailAddress]
         public string Password { get; set; }
+        [Required]
+        [RegularExpression("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}$", ErrorMessage = "Password must be complex")]
         public string DisplayName { get; set; }
+        [Required]
         public string Username { get; set; }
     }
 }
