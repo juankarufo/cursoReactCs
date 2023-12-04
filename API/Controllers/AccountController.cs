0a62e59b074117fc1f244106d5af8fb121440ffa
Juan Carlos de la Coba <jdel@ciklum.com>
2023-12-04 18:52:02 +0100

    s12 - registering new users

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -4,6 +4,7 @@ using Domain;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -42,6 +43,43 @@ namespace API.Controllers
             }
 
             return Unauthorized();
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<UserDto>> Register (RegistrerDto registrerDto)
+        {
+            if (await _userManager.Users.AnyAsync( x => x.UserName == registrerDto.Username))
+            {
+                return BadRequest("Username already taken");
+            }
+
+            if (await _userManager.Users.AnyAsync(x => x.Email == registrerDto.Email))
+            {
+                return BadRequest("Email already taken");
+            }
+
+            var user = new AppUser
+            {
+                DisplayName = registrerDto.DisplayName,
+                Email = registrerDto.Email,
+                UserName = registrerDto.Username
+            };
+
+            var result = await _userManager.CreateAsync(user, registrerDto.Password);
+
+            if(result.Succeeded)
+            {
+                return new UserDto
+                {
+                    DisplayName = user.DisplayName,
+                    Image = null,
+                    Token = _tokenService.CreateToken(user),
+                    Username = user.UserName
+                };
+            }else
+            {
+                return BadRequest(result.Errors);
+            }
         }
     }
 }
