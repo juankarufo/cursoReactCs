91cad09412afabafece529d7ba87f76738586652
Juan Carlos de la Coba <jdel@ciklum.com>
2023-12-18 19:20:14 +0100

    s14 - return activityDto (update automapper for linked items) 2/2
    We add more logic to the automapper. Automapper is inyected and used in List and Details hadlers now

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -1,5 +1,6 @@
 ﻿using Application.Core;
 using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using Domain;
 using MediatR;
 using Microsoft.AspNetCore.Http.HttpResults;
@@ -28,8 +29,7 @@ namespace Application.Activities
             public async Task<Result<List<ActivityDto>>> Handle(Query request, CancellationToken cancellationToken)
             {
                 var activities = await _context.Activities
-                    .Include(a => a.Attendees)
-                    .ThenInclude(u => u.AppUser)
+                    .ProjectTo<ActivityDto>(_mapper.ConfigurationProvider)
                     .ToListAsync(cancellationToken);
 
                 var activitiesToReturn = _mapper.Map<List<ActivityDto>>(activities);
