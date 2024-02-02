7118d25419bc5b7b637453500771a986598df9fa
Juan Carlos de la Coba <jdel@ciklum.com>
2024-02-02 13:38:43 +0100

    s16-adding cloudinary

---
+++ Infrastructure/Photos/CloudinarySettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Infrastructure.Photos
+{
+    public class CloudinarySettings
+    {
+        public string CloudNme { get; set; }
+        public string ApiKey { get; set; }
+        public string ApiSecret { get; set; }
+    }
+}
