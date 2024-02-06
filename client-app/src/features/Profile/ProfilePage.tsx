955f6babce1166303be1f90f320b3459449818bd
Juan Carlos de la Coba <jdel@ciklum.com>
2024-02-06 14:17:02 +0100

    s17 - adding profile header

--- client-app/src/features/Profile/ProfilePage.tsx
+++ client-app/src/features/Profile/ProfilePage.tsx
@@ -1,6 +1,12 @@
+import { Grid } from "semantic-ui-react";
+import ProfileHeader from "./ProfileHeader";
 
 export default function ProfilePage() {
     return (
-        <h1>Profile</h1>
+        <Grid>
+            <Grid.Column>
+                <ProfileHeader />
+            </Grid.Column>
+        </Grid>
     )
 }
